ee0f6345cdcf8473775776c6aa5e6746ac15a045
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-08-20 22:46:09 -0600

    sets up the basic keypad as described and the EyesOnMe component skeleton

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,17 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    function myChangeListener(event)
+    {
+        console.log("Entering password...");
+        console.log(event.target.value);
+    }
+
     return (
-        <div></div>
+        <div>
+            <input id="mpass" type="password" onChange={myChangeListener} />
+        </div>
     )
 }
 
